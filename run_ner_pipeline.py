b9efe1e27b519f27d29e5751c8947e096b9d72d7
Adrian <asanchez294@uoc.edu>
2025-03-02 17:36:14 +0100

    Argumentos añadidos para diferentes tipos de ejecuciones

--- run_ner_pipeline.py
+++ run_ner_pipeline.py
@@ -1,80 +1,102 @@
+import argparse
 import time
+
+from datasets import load_from_disk
 
 from src.data_preprocessing.ner_preprocess import NERPreprocessor
 from src.utils.config_loader import load_ner_config
 from src.utils.data_loader import create_hf_dataset_from_brats
-from src.utils.ner_utils import extract_label_maps
+from src.utils.ner_utils import extract_label_maps, load_label_maps
 
 from transformers import (
     AutoModelForTokenClassification,
     AutoTokenizer,
 )
 
-# Load NER general configs
-config = load_ner_config("config.yaml")
 
-# Paths from configuration file
-TRAIN_RAW = config["paths"]["ner"]["raw"]["train"]
-DEV_RAW = config["paths"]["ner"]["raw"]["dev"]
-TRAIN_PROCESSED = config["paths"]["ner"]["processed"]["train"]
-DEV_PROCESSED = config["paths"]["ner"]["processed"]["dev"]
+def __get_args():
+    # Manage arguments
+    parser = argparse.ArgumentParser(description="Tokenization and data preparation for fine-tuning EriBERTa")
+    parser.add_argument("--force_tokenize", action="store_true", help="Force to tokenize raw data")
+    return parser.parse_args()
 
-# Model to be trained
-medical_model = "HiTZ/EriBERTa-base"
-tokenizer = AutoTokenizer.from_pretrained(medical_model)
 
-# Load text+ann (BRAT) data into HF Dataset
-print("\n⏳ Loading data...")
-start_time = time.time()
-train_dataset = create_hf_dataset_from_brats(TRAIN_RAW)
-dev_dataset = create_hf_dataset_from_brats(DEV_RAW)
-print(f"✅ Data loaded in {time.time() - start_time:.2f} seconds.\n")
-# TODO Prueba (borrar cuando se compruebe)
-print(train_dataset[0])
+def run_ner_pipeline():
+    # Load NER general configs
+    config = load_ner_config("config.yaml")
 
-# Save HF Dataset to disk
-train_dataset.save_to_disk(TRAIN_PROCESSED)
-dev_dataset.save_to_disk(DEV_PROCESSED)
+    # Paths from configuration file
+    TRAIN_RAW = config["paths"]["ner"]["raw"]["train"]
+    DEV_RAW = config["paths"]["ner"]["raw"]["dev"]
+    TRAIN_PROCESSED = config["paths"]["ner"]["processed"]["train"]
+    DEV_PROCESSED = config["paths"]["ner"]["processed"]["dev"]
+    LABEL2ID_PATH = config["paths"]["ner"]["label_maps"]["label2id"]
 
-# Label maps
-print("\n⏳ Extracting label maps...")
-start_time = time.time()
-label2id, id2label = extract_label_maps(train_dataset)
-print(f"✅ Label maps extracted in {time.time() - start_time:.2f} seconds.\n")
-# TODO Prueba (borrar cuando se compruebe)
-print(label2id)
+    # Model to be trained
+    medical_model = "HiTZ/EriBERTa-base"
+    tokenizer = AutoTokenizer.from_pretrained(medical_model)
 
-# NERPreprocessor class
-preprocessor = NERPreprocessor(tokenizer, label2id, id2label)
+    # Arguments
+    args = __get_args()
 
-# Tokenize
-print("\n⏳ Tokenizing data...")
-start_time = time.time()
-tokenizer = AutoTokenizer.from_pretrained(medical_model)
-train_tokenized = preprocessor.process_dataset(train_dataset)
-dev_tokenized = preprocessor.process_dataset(dev_dataset)
-print(f"✅ Data tokenized in {time.time() - start_time:.2f} seconds.\n")
+    if args.force_tokenize:
 
-sample = train_tokenized[0]
-print("\nSample processed item:")
-for key, value in sample.items():
-    print(f"{key}: {value[:50]}...")
+        # Load text+ann (BRAT) data into HF Dataset
+        print("\n⏳ Loading raw data...")
+        start_time = time.time()
+        train_dataset = create_hf_dataset_from_brats(TRAIN_RAW)
+        dev_dataset = create_hf_dataset_from_brats(DEV_RAW)
+        print(f"✅ Data loaded in {time.time() - start_time:.2f} seconds.\n")
+        # TODO Prueba (borrar cuando se compruebe)
+        print(train_dataset[0])
 
-texto_original = train_dataset[0]["text"]
-for i, (offset, label) in enumerate(zip(sample["offset_mapping"][0], sample["labels"][0])):
-    inicio, fin = offset
-    fragmento = texto_original[inicio:fin]
-    print(f"Token {i}: '{fragmento}' -> Label: {label}")
+        # Label maps
+        print("\n⏳ Extracting label maps...")
+        start_time = time.time()
+        label2id, id2label = extract_label_maps(train_dataset, LABEL2ID_PATH)
+        print(f"✅ Label maps extracted in {time.time() - start_time:.2f} seconds.\n")
 
-# # Fine-tuning del modelo
-# print("\n Iniciando fine-tuning de EriBERTa...")
-# start_time = time.time()
-# fine_tune_model(train_data, dev_data, MODEL_DIR, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, epochs=EPOCHS)
-# print(f"✅ Fine-tuning completado en {time.time() - start_time:.2f} segundos.")
-#
-# # Evaluación del modelo
-# print("\n Evaluando el modelo...")
-# metrics = evaluate_model(MODEL_DIR, test_data)
-# print("Resultados de evaluación:", metrics)
-#
-# print("\n ¡Pipeline completado con éxito!")
+        # NERPreprocessor class
+        preprocessor = NERPreprocessor(tokenizer, label2id, id2label)
+
+        # Tokenize
+        print("\n⏳ Tokenizing data...")
+        start_time = time.time()
+        train_tokenized = preprocessor.process_dataset(train_dataset)
+        dev_tokenized = preprocessor.process_dataset(dev_dataset)
+        # Save tokenized data to disk
+        train_tokenized.save_to_disk(TRAIN_PROCESSED)
+        dev_tokenized.save_to_disk(DEV_PROCESSED)
+        print(f"✅ Data tokenized in {time.time() - start_time:.2f} seconds.\n")
+
+    else:
+
+        print("\n⏳ Loading processed data...")
+        start_time = time.time()
+        # Load processed data
+        train_tokenized = load_from_disk(TRAIN_PROCESSED)
+        dev_tokenized = load_from_disk(DEV_PROCESSED)
+        # Load label maps
+        label2id, id2label = load_label_maps(LABEL2ID_PATH)
+        print(f"✅ Data loaded in {time.time() - start_time:.2f} seconds.\n")
+        # TODO Prueba (borrar cuando se compruebe)
+        print(train_tokenized[0])
+        print(label2id)
+
+
+    # # Fine-tuning del modelo
+    # print("\n Iniciando fine-tuning de EriBERTa...")
+    # start_time = time.time()
+    # fine_tune_model(train_data, dev_data, MODEL_DIR, batch_size=BATCH_SIZE, learning_rate=LEARNING_RATE, epochs=EPOCHS)
+    # print(f"✅ Fine-tuning completado en {time.time() - start_time:.2f} segundos.")
+    #
+    # # Evaluación del modelo
+    # print("\n Evaluando el modelo...")
+    # metrics = evaluate_model(MODEL_DIR, test_data)
+    # print("Resultados de evaluación:", metrics)
+    #
+    # print("\n ¡Pipeline completado con éxito!")
+
+
+if __name__ == "__main__":
+    run_ner_pipeline()
--- src/utils/ner_utils.py
+++ src/utils/ner_utils.py
@@ -1,21 +1,61 @@
-def extract_label_maps(dataset, unique_labels=None):
+import json
+import os
 
-    if unique_labels is None:
+
+def extract_label_maps(dataset, label2id_path):
+
+    if os.path.exists(label2id_path):
+        # Get label map from path
+        label2id = load_label_map(label2id_path)
+        print(f"ℹ️ File \"{label2id_path}\" already exists and will be used instead of generating a new one")
+
+    else:
+        # Extract labels from dataset
         unique_labels = set()
 
-        # Extract labels from dataset
         for example in dataset:
             for entity in example["entities"]:
                 unique_labels.add(entity["label"])
 
-    # Sort for consistent ordering
-    unique_labels = sorted(unique_labels)
+        # Sort for consistent ordering
+        unique_labels = sorted(unique_labels)
 
-    label2id = {"O": 0}
-    for i, entity in enumerate(unique_labels, start=1):
-        label2id[f"B-{entity}"] = i * 2 - 1
-        label2id[f"I-{entity}"] = i * 2
+        # label2id map
+        label2id = {"O": 0}
+        for i, entity in enumerate(unique_labels, start=1):
+            label2id[f"B-{entity}"] = i * 2 - 1
+            label2id[f"I-{entity}"] = i * 2
 
+        # Save label2id map
+        save_label_map(label2id, label2id_path)
+
+    # id2label map
+    id2label = {i: label for label, i in label2id.items()}
+
+    return label2id, id2label
+
+
+def save_label_map(label_map, label_map_path):
+    with open(label_map_path, "w", encoding="utf-8") as file:
+        json.dump(label_map, file, indent=4)
+    print(f"ℹ️ File \"{label_map_path}\" generated.")
+
+
+def load_label_map(label_map_path):
+    if not os.path.exists(label_map_path):
+        raise FileNotFoundError(f"❌ File \"{label_map_path}\" is missing. "
+                                f"Run the full process to generate it or add the file manually.")
+
+    with open(label_map_path, "r", encoding="utf-8") as file:
+        label_map = json.load(file)
+    return label_map
+
+
+def load_label_maps(label2id_map_path):
+    # label2id map
+    label2id = load_label_map(label2id_map_path)
+
+    # id2label map
     id2label = {i: label for label, i in label2id.items()}
 
     return label2id, id2label
