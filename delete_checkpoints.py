65ec15ad5eef8b002c885e5c1b2b6728e6b1ee87
Adrian <asanchez294@uoc.edu>
2025-03-26 13:44:55 +0100

    Corrección

--- delete_checkpoints.py
+++ delete_checkpoints.py
@@ -15,6 +15,7 @@ def __get_args():
     parser = argparse.ArgumentParser(description="Tokenization and data preparation for fine-tuning EriBERTa")
     parser.add_argument("--model", type=str, default="eriberta", choices=["eriberta"], help="Model to use")
     parser.add_argument("--config_path", type=str, default="config/config_casimedicos_ner.yaml", help="Config path")
+    parser.add_argument("--folder_type", type=str, default="run", choices=["run", "wandb"], help="Checkpoints folder type")
     return parser.parse_args()
 
 
@@ -31,8 +32,12 @@ def delete_huggingface_checkpoints():
     # RESULTS_PATH from configuration file
     RESULTS_PATH = config["models"][arg_model]["results"]
 
-    # Get all run directories (e.g., run_0, run_1, etc.)
-    run_folders = [os.path.join(RESULTS_PATH, f) for f in os.listdir(RESULTS_PATH) if re.match(r'run_\d+', f)]
+    # Get all run directories (e.g., run_0, run_1, etc.) or wandb folder
+    folder_type = args.folder_type
+    if folder_type == "run":
+        run_folders = [os.path.join(RESULTS_PATH, f) for f in os.listdir(RESULTS_PATH) if re.match(r'run_\d+', f)]
+    else:
+        run_folders = [os.path.join(RESULTS_PATH, f) for f in os.listdir(RESULTS_PATH) if re.match('wandb', f)]
     checkpoint_folders = []
 
     # Search for checkpoint folders inside each run directory
@@ -54,7 +59,7 @@ def delete_huggingface_checkpoints():
             for dir in dirs:
                 os.rmdir(os.path.join(root, dir))
         os.rmdir(folder)  # Finally, remove the checkpoint folder itself
-    logger.info(f"Checkpoints from {RESULTS_PATH} successfully deleted.")
+    logger.info(f"{folder_type} checkpoints from {RESULTS_PATH} successfully deleted.")
 
 
 if __name__ == "__main__":
